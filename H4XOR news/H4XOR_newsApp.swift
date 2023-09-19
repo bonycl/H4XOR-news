5b9af52f099cbd59beeffa8341d2b73ae8ee92b0
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-09-19 18:52:08 +0700

    Initial Commit

---
+++ H4XOR news/H4XOR_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_newsApp.swift
+//  H4XOR news
+//
+//  Created by D i on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
